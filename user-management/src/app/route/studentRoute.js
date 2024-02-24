747d0bc8300a28f3486358370cc903908bf61825
MaiUoc <tinhmai775@gmail.com>
2024-02-24 18:40:34 +0700

    Update order API

--- user-management/src/app/route/studentRoute.js
+++ user-management/src/app/route/studentRoute.js
@@ -6,10 +6,10 @@ const Authorize = require('../middleware/studentAuth');
 const Photo = require('../../config/firebase/photo');
 
 router.route('/').get(StudentController.getAllStudent);
+router.route('/get-student-by-email').get(StudentController.getStudentByEmail);
 router.route('/:studentId')
     .get(StudentController.getStudentById)
     .put(Authorize.protectedAPI, StudentController.updateStudent);
-router.route('/get-student-by-email').get(StudentController.getStudentByEmail);
 router.route('/change-password').put(Authorize.protectedAPI, StudentController.changePassword);
 router.route('/forgot-password').post(StudentController.forgotPassword);
 router.route('/reset-password/:resetToken').put(StudentController.resetPassword);
