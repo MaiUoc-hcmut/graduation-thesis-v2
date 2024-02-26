da211f11423689d9c76f750e2627df18fa904296
MaiUoc <tinhmai775@gmail.com>
2024-02-26 21:07:30 +0700

    Update table review

---
+++ service-course/src/db/migrations/20240226140551-update-table-review.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('review', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      id_student: {
+        type: Sequelize.UUID,
+      },
+      id_teacher: {
+        type: Sequelize.UUID,
+      },
+      id_course: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'Course',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      id_exam: {
+        type: Sequelize.UUID,
+      },
+      content: {
+        type: Sequelize.TEXT
+      },
+      image: {
+        type: Sequelize.TEXT
+      },
+      rating: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('review');
+  }
+};
