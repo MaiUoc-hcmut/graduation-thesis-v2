d20d89b18c1932537df447528b0f4e223983243f
MaiUoc <tinhmai775@gmail.com>
2024-03-12 02:10:38 +0700

    Update type of period

--- service-exam/src/db/migrations/20240311104143-add-new-column.js
+++ service-exam/src/db/migrations/20240311104143-add-new-column.js
@@ -5,10 +5,16 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('exam', 'pass_score', {
       type: Sequelize.FLOAT,
-    })
+    });
+    await queryInterface.changeColumn('exam', 'period', {
+      type: Sequelize.INTEGER
+    });
   },
 
   async down (queryInterface, Sequelize) {
     await queryInterface.removeColumn('exam', 'pass_score');
+    await queryInterface.changeColumn('exam', 'period', {
+      type: Sequelize.TIME
+    });
   }
 };
