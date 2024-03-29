af82939c905fb220443bcbfdf192f82e93c9a087
MaiUoc <tinhmai775@gmail.com>
2024-03-29 18:52:16 +0700

    Create notification when created course and exam

--- service-notification/src/db/migrations/20240329112510-add-new-column-to-notification-table.js
+++ service-notification/src/db/migrations/20240329112510-add-new-column-to-notification-table.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addcolumn('notification', 'read', {
+    await queryInterface.addColumn('notification', 'read', {
       type: Sequelize.BOOLEAN
     });
   },
