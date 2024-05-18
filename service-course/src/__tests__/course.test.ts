6bba68940b3f752a7516c180de9e02dde06e0000
MaiUoc <tinhmai775@gmail.com>
2024-05-18 12:00:39 +0700

    Recover to default

--- service-course/src/__tests__/course.test.ts
+++
@@ -1,8 +0,0 @@
-import supertest from 'supertest';
-
-
-describe('get courses list', () => {
-    it('should return a list of courses', async () => {
-        const response = await supertest()
-    })
-});
